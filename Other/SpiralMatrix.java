4cb7c6584c36b31aa4463d0b9ad13cc6755d10da
Yuanchao Yu <Chaos@Yuanchaos-MacBook-Pro.local>
2015-10-28 21:28:20 -0400

    121

---
+++ Other/SpiralMatrix.java
@@ -0,0 +1,55 @@
+package Algorithm.Other;
+
+import java.util.*;
+
+//Question: https://leetcode.com/problems/spiral-matrix/
+public class SpiralMatrix{
+	public static List<Integer> spiralOrder(int[][] nums){
+		//Similar idea as rotate image
+		//we divide them into different layer
+		List<Integer> res = new ArrayList<Integer>();
+		if(nums == null||nums.length == 0|| nums[0].length== 0){
+			return res;
+		}
+		//Because the array is not necessarly n*n
+		//so the number of layer is determined by the smaller width or length;
+		int min = Math.min(nums.length,nums[0].length);
+		int layerNum = min/2;
+		for(int layer = 0;layer<layerNum;layer++){
+			for(int i = layer;i<nums[0].length-1-layer;i++){
+				res.add(nums[layer][i]);
+			}
+			for(int i = layer;i<nums.length-1-layer;i++){
+				res.add(nums[i][nums[0].length-1-layer]);
+			}
+			for(int i = layer;i<nums[0].length-1-layer;i++){
+				res.add(nums[nums.length-1-layer][nums[0].length-1-i]);
+			}
+			for(int i = layer;i<nums.length-1-layer;i++){
+				res.add(nums[nums.length-1-i][layer]);
+			}
+		}
+		//if layer is odd, we need to add one remain row or col
+		if(min%2 ==1 ){
+			if(min == nums.length){
+				for(int i = layerNum;i< nums[0].length-layerNum;i++){
+					res.add(nums[layerNum][i]);
+				}
+			}else{
+				for(int i = layerNum;i<nums.length-layerNum;i++){
+					res.add(nums[i][layerNum]);
+				}
+			}
+		}
+		return res;
+	}
+
+	public static void main(String[] args){
+		int[][] nums = {{1,2,3},{4,5,6},{7,8,9}};
+		List<Integer> res = spiralOrder(nums);
+		for(Integer num: res){
+			System.out.print(num+" ");
+		}
+	}
+
+}
---
+++ String/LengthofLastWord.java
@@ -0,0 +1,27 @@
+package Algorithm.String;
+
+public class LengthofLastWord{
+
+	public static int lengthOfLastWord(String s) {
+		//corner cases
+        if(s == null || s.length() == 0){
+            return 0;
+        }
+        //basically we just use to pointer to mark the start of last word
+        //and the end of the last word
+        int lastEnd = s.length()-1;
+        //igonore the space at the end of the array.
+        while(lastEnd >= 0 &&s.charAt(lastEnd) == ' ' ){
+            lastEnd--;
+        }
+        //corner case, when there is only space
+        if(lastEnd <0){
+            return 0;
+        }
+        int lastStart = lastEnd;
+        while(lastStart >=0 &&s.charAt(lastStart) != ' '){
+            lastStart--;
+        }
+        return lastEnd-lastStart;
+    }
+}
