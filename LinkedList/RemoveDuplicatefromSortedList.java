7e9ba2d4caa948207937f8ad73ba1fd37df0ab11
Yuanchao Yu <Chaos@Yuanchaos-MacBook-Pro.local>
2015-11-05 16:35:39 -0500

    RemoveDuplicateSortedList

---
+++ LinkedList/RemoveDuplicatefromSortedList.java
@@ -0,0 +1,22 @@
+package Algorithm.LinkedList;
+
+public class RemoveDuplicatefromSortedList{
+	 public ListNode deleteDuplicates(ListNode head) {
+        if(head == null || head.next == null){
+            return head;
+        }
+        ListNode pre = head;
+        ListNode cur = head.next;
+        while(cur != null){
+            if(cur.val == pre.val){
+                cur = cur.next;
+            }else{
+                pre.next = cur;
+                pre = cur;
+                cur = cur.next;
+            }
+        }
+        pre.next = null;
+        return head;
+    }
+}
