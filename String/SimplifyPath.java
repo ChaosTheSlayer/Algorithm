5571cd4c445be3ffdaed68b388ec700e2f4b8267
Yuanchao Yu <Chaos@Yuanchaos-MacBook-Pro.local>
2015-11-07 12:35:59 -0500

    InterleavingString

--- String/SimplifyPath.java
+++ String/SimplifyPath.java
@@ -2,12 +2,7 @@ package Algorithm.String;
 
 //Question:https://leetcode.com/problems/simplify-path/
 
-// 这道题目是Linux内核中比较常见的一个操作，就是对一个输入的文件路径进行简化。
-// 思路比较明确，就是维护一个栈，对于每一个块（以‘/’作为分界）进行分析，
-// 如果遇到‘../’则表示要上一层，那么就是进行出栈操作，如果遇到‘./’则是停留当前，直接跳过，
-// 其他文件路径则直接进栈即可。最后根据栈中的内容转换成路径即可（这里是把栈转成数组，然后依次添加）。
-// 时间上不会超过两次扫描（一次是进栈得到简化路径，一次是出栈获得最后结果），所以时间复杂度是O(n)，
-// 空间上是栈的大小，也是O(n)。代码如下：
+// `
 
 public class SimplifyPath{
 	  public String simplifyPath(String path) {
---
+++ String/StringPermutation.java
@@ -0,0 +1,66 @@
+import java.util.*;
+
+public class StringPermutation{
+
+	public static List<String> permutation(String str){
+		List<String> res = new ArrayList<String>();
+		if(str == null ||str.length() == 0){
+			return res;
+		}
+		boolean[] visited = new boolean[str.length()];
+		char[] charArray = str.toCharArray();
+		helper(res,visited,charArray,new StringBuilder());
+		return res;
+	}
+
+	private static void helper(List<String> res, boolean[] visited,char[] charArray,StringBuilder temp){
+		if(temp.length() == charArray.length){
+			res.add(temp.toString());
+			return;
+		}
+
+		for(int i = 0;i<charArray.length;i++){
+			if(!visited[i]){
+				visited[i] = true;
+				temp.append(charArray[i]);
+				helper(res,visited,charArray,temp);
+				temp.deleteCharAt(temp.length()-1);
+				visited[i] = false;
+			}
+		}
+	}
+
+	public static List<String> permutationIteration(String str){
+		List<String> res = new ArrayList<String>();
+		if(str == null || str.length() == 0){
+			return res;
+		}
+		List<StringBuilder> temp = new ArrayList<StringBuilder>();
+		temp.add(new StringBuilder());
+		temp.get(0).append(str.charAt(0));
+		for(int i = 1;i<str.length();i++){
+			List<StringBuilder> curRes = new ArrayList<StringBuilder>();
+			for(int j= 0;j<temp.size();j++){
+				StringBuilder cur = temp.get(j);
+				for(int k = 0;k<cur.length()+1;k++){
+					StringBuilder item = new StringBuilder(cur.toString());
+					item.insert(k,str.charAt(i));
+					curRes.add(item);
+				}
+			}
+			temp = curRes;
+		}
+		for(StringBuilder sb: temp){
+			res.add(sb.toString());
+		}
+		return res;
+	}
+
+	public static void main(String[] args){
+		List<String> res = permutationIteration("4321");
+		for(String s :res){
+			System.out.println(s);
+		}
+		System.out.println("The size of result is " + res.size());
+	}
+}
